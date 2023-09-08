bf5552dc6a7b9f6c516b66b7ba3ba617da3da4cd
Sridevi2323 <144429648+Sridevi2323@users.noreply.github.com>
2023-09-08 13:49:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def_is leap year (year):
+      If (year/4=0 and year /100) or year 400=0
+  Return true
+else :
+    Return False
+Year =2012
+If is leap year (year):
+Print ("() is a leap year ", formet (year ))
+else :
+       Print ("() is not a leap year ", formet (year))
